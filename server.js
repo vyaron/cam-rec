5f0a5f836a50340435322dc2aa00bce9d99b5b31
vyaron <vyaron@gmail.com>
2019-06-06 23:11:34 +0300

    initial

---
+++ server.js
@@ -0,0 +1,26 @@
+const multipart = require("connect-multiparty");
+const multipartMiddleware = multipart();
+const path = require("path");
+const os = require("os");
+const fs = require("fs");
+
+var express = require("express");
+var app = express();
+app.use(express.static("."));
+
+app.get("/", function(req, res) {
+  res.redirect("/index.html");
+});
+
+app.post("/", multipartMiddleware, function(req, res) {
+  console.log("files", req.files.data.path);
+  let location = path.join(os.tmpdir(), `cam-rec-${Date.now()}.webm`);
+  fs.rename(req.files.data.path, location, ()=>{
+      console.log(`upload successful, file written to ${location}`);
+      res.send(`upload successful, file written to ${location}`);
+  });
+});
+
+app.listen(3000, function() {
+  console.log("Example app listening on port 3000!");
+});
